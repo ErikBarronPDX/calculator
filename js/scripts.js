71351b77620caefe377220d081338fda974c16f5
Erik Barron and jason stocks <erik@>
2020-01-16 15:58:08 -0800

    use branching to determine the operator done to the input numbers

--- js/scripts.js
+++ js/scripts.js
@@ -17,59 +17,22 @@ var divide = function(number1, number2) {
 
 //UI Logic
 $(document).ready(function(){
-
-  $("form#add").submit(function(event){
+  $("form#math").submit(function(event){
     event.preventDefault();
-    var addNumber1 = parseInt($("#add1").val());
-    var addNumber2 = parseInt($("#add2").val());
-    var addResult = add(addNumber1, addNumber2);
-    $("#addOutput").text(addResult);
+    var number1 = parseInt($("#math1").val());
+    var number2 = parseInt($("#math2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+
+
+    if (operator === "1"){
+      var result = add(number1, number2);
+    } else if (operator === "2"){
+      var result = subtract(number1, number2);
+    } else if (operator === "3") {
+      var result = multiply(number1, number2);
+    } else {
+      var result = divide(number1, number2);
+    }
+    $("#mathOutput").text(result);
   });
-
-
-  $("form#subtract").submit(function(event){
-    event.preventDefault();
-    var subtractNumber1 = parseInt($("#subtract1").val());
-    var subtractNumber2 = parseInt($("#subtract2").val());
-    var subtractResult = subtract(subtractNumber1, subtractNumber2);
-    $("#subtractOutput").text(subtractResult);
-  })
-
-
-  $("form#multiply").submit(function(event){
-    event.preventDefault();
-    var multiplyNumber1 = parseInt($("#multiply1").val());
-    var multiplyNumber2 = parseInt($("#multiply2").val());
-    var multiplyResult = multiply(multiplyNumber1, multiplyNumber2);
-    $("#multiplyOutput").text(multiplyResult);
-  })
-
-
-  $("form#divide").submit(function(event){
-    event.preventDefault();
-    var divideNumber1 = parseInt($("#divide1").val());
-    var divideNumber2 = parseInt($("#divide2").val());
-    var divideResult = divide(divideNumber1, divideNumber2);
-    $("#divideOutput").text(divideResult);
-  })
-
 });
-
-
-/*
-var number1 = parseInt(prompt("Enter a number: "));
-
-var number2 = parseInt(prompt("Enter another number: "));
-
-var result = subtract(number1, number2);
-
-
-alert(result);
-
-BMI Calulator
-var height = parseInt(prompt("Enter your heights in inches: "));
-var weight = parseInt(prompt("Enter your weight with pounds: "));
-var BMI    = ( weight / (height * height)) * 703;
-
-alert(BMI);
-*/
